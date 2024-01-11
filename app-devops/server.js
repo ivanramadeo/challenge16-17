46170cf9b2a2df70e10e115d64d5f84f9aed0650
Ivan Leonardo Ramírez <ivanramirez.deo@gmail.com>
2024-01-11 18:36:46 -0300

    Commit sobre testing

--- app-devops/server.js
+++ app-devops/server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('¡Hola Mundo! Desde DevOps!');
 });
 
-app.listen(port, () => {
-  console.log(`Servidor ejecutándose en http://localhost:${port}`);
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${port}`);
+  });
+}
+// Export para testeo
+module.exports = app;
--- app-devops/server.test.js
+++ app-devops/server.test.js
@@ -1,10 +1,21 @@
-// Importar las dependencias necesarias para tus pruebas
 const request = require('supertest');
-const app = require('./app'); // Asegúrate de importar correctamente tu aplicación Express
+const app = require('./server'); 
 
 describe('Pruebas básicas de endpoint', () => {
+    let server;
+
+  // Antes de todas las pruebas, inicia el servidor
+    beforeAll((done) => {
+    server = app.listen(3000, done);
+    });
+
     test('GET / debe responder con 200', async () => {
-    const response = await request(app).get('/');
+    const response = await request(server).get('/');
     expect(response.statusCode).toBe(200);
-});
+    });
+
+  // Después de todas las pruebas, cierra el servidor
+    afterAll((done) => {
+    server.close(done);
+    });
 });
